e9d2f19501263164f41e5c832128c1caec1c0526
manasa-bubbly <manasakrishnareddy1031@gmail.com>
2025-06-12 15:30:47 +0530

    Add files via upload

---
+++ Memoization.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+public class Memoization{
+    private HashMap<Integer,Integer> memo=new HashMap<>();
+    public int fibMemo(int n){
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        if(n<=1){
+            return n;
+        }
+        int result=fibMemo(n-1)+fibMemo(n-2);
+        memo.put(n,result);
+        return result;
+    }
+    public static void main(String[] args){
+        Fibonacci fibonacci=new Fibonacci();
+        int n=10;
+        System.out.println("Fibonacci of" + n + "is:" + fibonacci.fibMemo(n));
+    }
+}
